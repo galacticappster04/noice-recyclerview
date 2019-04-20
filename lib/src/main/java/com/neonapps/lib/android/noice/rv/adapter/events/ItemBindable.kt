01fc91ea733e7eb77fae9b62ea6cf038c09fbcff
Jose Jerson Naval Jr <jnaval04@hotmail.com>
2019-04-20 17:20:06 +0800

    Provide default implementations

--- lib/src/main/java/com/neonapps/lib/android/noice/rv/adapter/events/ItemBindable.kt
+++ lib/src/main/java/com/neonapps/lib/android/noice/rv/adapter/events/ItemBindable.kt
@@ -4,5 +4,5 @@ import androidx.recyclerview.widget.RecyclerView
 import com.neonapps.lib.android.noice.rv.adapter.AppSimpleAdapter
 
 interface ItemBindable<V> {
-    fun bind(adapter : AppSimpleAdapter<V>, holder : RecyclerView.ViewHolder, position : Int)
+    fun bind(adapter : AppSimpleAdapter<V>, holder : RecyclerView.ViewHolder, position : Int){}
 }
--- lib/src/main/java/com/neonapps/lib/android/noice/rv/adapter/events/ItemClickable.kt
+++ lib/src/main/java/com/neonapps/lib/android/noice/rv/adapter/events/ItemClickable.kt
@@ -1,5 +1,5 @@
 package com.neonapps.lib.android.noice.rv.adapter.events
 
 interface ItemClickable<V> {
-    fun click(visitor : V, position : Int, eventName: String)
+    fun click(visitor : V, position : Int, eventName: String){}
 }
