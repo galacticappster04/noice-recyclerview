6e94c7d5231fac0a5d34bbaf2f917f81ca3c6586
Jose Jerson Naval Jr <jnaval04@hotmail.com>
2019-07-21 15:40:17 +0800

    Provide callback when an item has been bound

--- lib/src/main/java/com/neonapps/lib/android/noice/rv/adapter/events/ItemBindable.kt
+++ lib/src/main/java/com/neonapps/lib/android/noice/rv/adapter/events/ItemBindable.kt
@@ -2,7 +2,10 @@ package com.neonapps.lib.android.noice.rv.adapter.events
 
 import androidx.recyclerview.widget.RecyclerView
 import com.neonapps.lib.android.noice.rv.adapter.AppSimpleAdapter
+import com.neonapps.lib.android.noice.rv.adapter.item.AppSimpleAdapterItem
 
 interface ItemBindable<V> {
     fun bind(adapter : AppSimpleAdapter<V>, holder : RecyclerView.ViewHolder, position : Int){}
+
+    fun onBound(holder : RecyclerView.ViewHolder, item : AppSimpleAdapterItem<V>, position : Int)
 }
