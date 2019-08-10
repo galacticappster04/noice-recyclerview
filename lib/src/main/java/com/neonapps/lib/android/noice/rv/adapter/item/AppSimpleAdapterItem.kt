ea750ded269cf27521d4500740925e24370ddb54
Jose Jerson Naval Jr <jnaval04@hotmail.com>
2019-08-11 01:30:43 +0800

    Fix calling getItemId causing it to create a new id everytime

--- lib/src/main/java/com/neonapps/lib/android/noice/rv/adapter/item/AppSimpleAdapterItem.kt
+++ lib/src/main/java/com/neonapps/lib/android/noice/rv/adapter/item/AppSimpleAdapterItem.kt
@@ -3,5 +3,8 @@ package com.neonapps.lib.android.noice.rv.adapter.item
 import com.neonapps.lib.android.noice.rv.adapter.AppSimpleAdapter
 
 abstract class AppSimpleAdapterItem<V> : AdapterItem<V> {
-    override fun getItemId(): Long = AppSimpleAdapter.getItemId()
+
+    protected val _itemId : Long = AppSimpleAdapter.getItemId()
+
+    override fun getItemId(): Long = _itemId
 }
