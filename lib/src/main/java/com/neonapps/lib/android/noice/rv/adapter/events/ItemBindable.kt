53c311c5d7f68008d09280cb7e6e990d92ae6169
Neon Warge <jnaval04@hotmail.com>
2020-07-25 15:30:27 +0800

    Remove onBound callback

--- lib/src/main/java/com/neonapps/lib/android/noice/rv/adapter/events/ItemBindable.kt
+++ lib/src/main/java/com/neonapps/lib/android/noice/rv/adapter/events/ItemBindable.kt
@@ -1,13 +1,9 @@
 package com.neonapps.lib.android.noice.rv.adapter.events
 
 import androidx.recyclerview.widget.RecyclerView
-import com.neonapps.lib.android.noice.rv.adapter.AppSimpleAdapter
-import com.neonapps.lib.android.noice.rv.adapter.item.AdapterItem
 
 interface ItemBindable<V> {
-    fun bind(holder : RecyclerView.ViewHolder, position : Int){}
-
-    fun onBound(visitor : V, holder : RecyclerView.ViewHolder, position : Int, eventName : String = "") {}
+    fun bind(holder : RecyclerView.ViewHolder, visitor : V?, position : Int){}
 
     fun onViewDetachedFromWindow(holder: RecyclerView.ViewHolder) {}
 }
