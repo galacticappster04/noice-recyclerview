8a1e529a575c9ccdea506fd1b413beac06f99136
Neon Warge <jnaval04@hotmail.com>
2020-07-25 15:32:20 +0800

    Remove PrototypeProvider

--- lib/src/main/java/com/neonapps/lib/android/noice/rv/adapter/events/PrototypeProvider.kt
+++
@@ -1,7 +0,0 @@
-package com.neonapps.lib.android.noice.rv.adapter.events
-
-import com.neonapps.lib.android.noice.rv.adapter.holder.TypedHolder
-
-interface PrototypeProvider {
-    fun createPrototype() : TypedHolder.Prototype
-}
